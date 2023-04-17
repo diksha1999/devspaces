69291f2dc538d699b994b8ce20e11485ab3e0826
Diksha Sharma <1740dikshasharma@gmail.com>
2023-04-18 00:14:45 +0530

    Revert "testing variables"
    This reverts commit a58a8920900b32357e8561086c5e6b34e8e24e75.

--- terraform/hello-world/abc.tf
+++
@@ -1,3 +0,0 @@
-output "abcoutputblock"{
-	value="This is abc block"
-}
--- terraform/hello-world/variable/hello-variable.tf
+++
@@ -1,9 +0,0 @@
-
-output printname{
-    value = "Hello, ${var.username}!! Your age is ${var.age}"
-}
-
-output testlist{
-    value="First user is ${var.users[0]}"
-}
-
